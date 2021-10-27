489d80700e9a3bd7cfac0d564c6ba8ee99634ba2
unknown <jacobmarcosoliver@gmail.com>
2021-10-28 03:17:45 +0330

    auto time and date added to order.service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,3 +1,4 @@
+import * as moment from 'jalali-moment';
 import { Order } from './entities/order.entity';
 import { Food } from './../foods/entities/food.entity';
 import { Connection, getRepository, Repository } from 'typeorm';
@@ -14,9 +15,6 @@ export class OrdersService {
   ){}
 
   async create(createOrderInput: CreateOrderInput) {
-    const getRepositoryFood = getRepository(Food);
-    const getRepositoryOrder = getRepository(Order);
-    //================================ with transaction ===========================================
     console.log("(with transaction) Inserting a new order with its own foods into database.");
     const t1 = Date.now()
     const queryRunner = this.connection.createQueryRunner();
@@ -25,8 +23,8 @@ export class OrdersService {
     try {
       const NewTempOrder = new Order();
       NewTempOrder.name = createOrderInput.name;
-      NewTempOrder.date = createOrderInput.date;
-      NewTempOrder.time = createOrderInput.time;
+      NewTempOrder.date = moment().locale('fa').format('YYYY/M/D');
+      NewTempOrder.time = moment().format('HH:mm');
       const listTempFoods = [];
       for (const eachFoodFromGraphQl of createOrderInput.foods) {
         const NewTempFood = new Food();
@@ -38,11 +36,12 @@ export class OrdersService {
         listTempFoods.push(NewTempFood);
       }
       NewTempOrder.foods = listTempFoods;
-      await queryRunner.manager.save(NewTempOrder);
+      const PromiseOrder = await queryRunner.manager.save(NewTempOrder);
       await queryRunner.commitTransaction();
       const t2 = Date.now()
       const delta_time = (t2-t1) / 1000;
       console.log(`=========== inserted after ${delta_time} sec ===============`);
+      return PromiseOrder;
     } catch (err) {
       // since we have errors lets rollback the changes we made
       await queryRunner.rollbackTransaction();
@@ -50,29 +49,6 @@ export class OrdersService {
       // you need to release a queryRunner which was manually instantiated
       await queryRunner.release();
     }
-    //================================= without transaction =======================================
-    // console.log("(without transaction) Inserting a new order with its own foods into database.");
-    // const t1 = Date.now()
-    // const NewTempOrder = new Order();
-    // NewTempOrder.name = createOrderInput.name;
-    // NewTempOrder.date = createOrderInput.date;
-    // NewTempOrder.time = createOrderInput.time;
-    // const listTempFoods = [];
-    // for (const eachFoodFromGraphQl of createOrderInput.foods) {
-    //   const NewTempFood = new Food();
-    //   NewTempFood.name = eachFoodFromGraphQl.name;
-    //   NewTempFood.price = eachFoodFromGraphQl.price;
-    //   NewTempFood.is_subsidy = eachFoodFromGraphQl.is_subsidy;
-    //   NewTempFood.subsidy_price = eachFoodFromGraphQl.subsidy_price;
-    //   listTempFoods.push(NewTempFood);
-    //   await getRepositoryFood.save(NewTempFood);
-    // }
-    // NewTempOrder.foods = listTempFoods;
-    // const resultAfterSaveNewOrder = await getRepositoryOrder.save(NewTempOrder);
-    // const t2 = Date.now()
-    // const delta_time = (t2-t1) / 1000;
-    // console.log(`=========== inserted after ${delta_time} sec ===============`);
-    // return resultAfterSaveNewOrder;
   }
 
   findAll() {
